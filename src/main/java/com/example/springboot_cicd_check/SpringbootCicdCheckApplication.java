2f02003e3f04389cd3bded15f64269e447e92e63
Jenish <hello@jenish.net>
2024-08-01 13:17:51 +0530

    Checking

---
+++ src/main/java/com/example/springboot_cicd_check/SpringbootCicdCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cicd_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCicdCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_cicd_check/SpringbootCicdCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cicd_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
